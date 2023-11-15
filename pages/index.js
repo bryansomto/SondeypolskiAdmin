0a80274982a9a899ac1aad6dd1b93d4ddb36aef4
bryansomto <bryansomto@gmail.com>
2023-11-15 14:57:35 +0100

    Initial commit for admin app

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useSession } from "next-auth/react";
+import Layout from "../components/Layout";
+import Image from "next/image";
+
+export default function Home() {
+  const { data: session } = useSession();
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <Image
+            src={session?.user?.image}
+            alt={`An image of ${session?.user?.name}`}
+            width={24}
+            height={24}
+          />
+          <span className="px-2">{session?.user?.name}</span>
+        </div>
+      </div>
+    </Layout>
+  );
+}
