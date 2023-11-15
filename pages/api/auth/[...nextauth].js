2aae25e661b764c4617f4d475e43b3394621328a
bryansomto <bryansomto@gmail.com>
2023-11-15 16:40:32 +0100

    access modifications

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -20,8 +20,7 @@ export const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
     session: ({ session, token, user }) => {
-      console.log(process.env.ADMIN_EMAILS.split(", "));
-      if (process.env.ADMIN_EMAILS.includes(session?.user?.email)) {
+      if (process.env.ADMIN_EMAILS.split(", ").includes(session?.user?.email)) {
         return session;
       } else {
         return false;
@@ -34,7 +33,7 @@ export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
-  if (!process.env.ADMIN_EMAILS.includes(session?.user?.email)) {
+  if (!process.env.ADMIN_EMAILS.split(", ").includes(session?.user?.email)) {
     res.status(401);
     res.end();
     // throw "not an admin";
